f6e40e26fd562c74b841a78d3cb2743b5b9cab80
Justus Ohlhaver <ohlhaver@gmail.com>
2013-02-03 00:19:37 +0100

    small fix for category pages without type param

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,12 +3,14 @@ class CategoriesController < ApplicationController
 			@category = Category.find(params[:id])
 			if params[:type]
 				@type = Type.find(params[:type])
+				@type_events = @type.events.uniq.sort_by(&:time)
 				@events = category_events(@category).select { |event| event.type_id == @type.id}.uniq.sort_by(&:time)
 			else
 				@events = category_events(@category).uniq.sort_by(&:time)
+				@type_events = @events
 			end
 			
-			@type_events = @type.events.uniq.sort_by(&:time)
+			
 			@type_categories=[]
 
 			@type_events.each do |event|
