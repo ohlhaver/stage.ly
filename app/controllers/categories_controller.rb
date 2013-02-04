c7385d2ad4259c1013d89cac4f32d44191ee6bd9
Justus Ohlhaver <ohlhaver@gmail.com>
2013-02-04 23:40:13 +0100

    back to db query for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,18 @@
 class CategoriesController < ApplicationController
 	def show
 			@category = Category.find(params[:id])
-			@events = Event.search :conditions => {:categories => @category.name}, :match_mode => :any, :order => :time,
-		  :sort_mode => :asc
+			#@events = Event.search :conditions => {:categories => @category.name}, :match_mode => :any, :order => :time,
+		  #:sort_mode => :asc
 			if params[:type]
 				@type = Type.find(params[:type])
 			#	@type_events = @type.events.uniq.sort_by(&:time)
-			#	@events = category_events(@category).select { |event| event.type_id == @type.id}.uniq.sort_by(&:time)
+				@events = category_events(@category).select { |event| event.type_id == @type.id}.uniq.sort_by(&:time)
 						@type_categories = @type.categories#@type_categories.uniq.sort_by(&:name)
-			end
+			else
 			
-				#@events = category_events(@category).uniq.sort_by(&:time)
+				@events = category_events(@category).uniq.sort_by(&:time)
 			#	@type_events = @events
-			#end
+			end
 			
 			
 			#@type_categories=[]
