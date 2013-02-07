f11bca75e78dfa06395bd8fb4dc79e7cd8165ea0
Justus Ohlhaver <ohlhaver@gmail.com>
2013-02-07 02:57:46 +0100

    lots of small changes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -26,6 +26,51 @@ module EventsHelper
 				end
 	end
 
+	def german_month(month)
+		if month == "1"
+			return "Januar"
+		elsif month == "2"
+			return "Februar"
+		elsif month == "3"
+			return "Maerz"
+		elsif month == "4"
+			return "April"
+		elsif month == "5"
+			return "Mai"
+		elsif month == "6"
+			return "Juni"
+		elsif month == "7"
+			return "Juli"
+		elsif month == "8"
+			return "August"
+		elsif month == "9"
+			return "September"
+		elsif month == "10"
+			return "Oktober"
+		elsif month == "11"
+			return "November"
+		elsif month == "12"
+			return "Dezember"
+		end
+	end
+
+	def german_weekday(day)
+		if day == "0"
+			return "Sonntag"
+		elsif day == "1"
+			return "Montag"
+		elsif day == "2"
+			return "Dienstag"
+		elsif day == "3"
+			return "Mittwoch"
+		elsif day == "4"
+			return "Donnerstag"
+		elsif day == "5"
+			return "Freitag"
+		elsif day == "6"
+			return "Samstag"
+		end
+	end
 		
 
 end
